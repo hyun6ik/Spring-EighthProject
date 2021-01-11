6ecc4aa9166133315e229437954fa8f5f0d51d51
hyun6ik <fkanwm@naver.com>
2021-01-11 22:14:07 +0900

    View 환경 설정

--- src/main/java/springeighthproject/spring_jpa/SpringJpaApplication.java
+++ src/main/java/springeighthproject/spring_jpa/SpringJpaApplication.java
@@ -8,10 +8,6 @@ public class SpringJpaApplication {
 
 	public static void main(String[] args) {
 
-		Hello hello = new Hello();
-		hello.setData("hello");
-		String data = hello.getData();
-		System.out.println("data = " + data);
 		SpringApplication.run(SpringJpaApplication.class, args);
 	}
 
