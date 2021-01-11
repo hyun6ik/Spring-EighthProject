9ffd493fc2b39258a54e6eac450104d17d1f95e4
hyun6ik <fkanwm@naver.com>
2021-01-12 01:02:20 +0900

    엔티티 클래스 개발2

--- src/main/java/springeighthproject/spring_jpa/domain/Address.java
+++ src/main/java/springeighthproject/spring_jpa/domain/Address.java
@@ -1,6 +1,7 @@
 package springeighthproject.spring_jpa.domain;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Embeddable;
@@ -11,9 +12,22 @@ import javax.persistence.Id;
 
 @Embeddable
 @Getter
+//값 타입은 Setter를 사용하지 않는것이 좋다
+// 생성자에서 갑승ㄹ 모두 초기화해서 변경 불가능한 클래스를 만드는 것이 좋다.
+// 또한 기본생성자를 protected로 설정해야 안전하다
 public class Address {
 
     private String city;
     private String street;
     private String zipcode;
+
+    protected Address(){
+
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
--- src/main/java/springeighthproject/spring_jpa/domain/item/Item.java
+++ src/main/java/springeighthproject/spring_jpa/domain/item/Item.java
@@ -21,5 +21,7 @@ public abstract class Item {
     private int price;
     private int stockQuantity;
 
+    @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
 }
