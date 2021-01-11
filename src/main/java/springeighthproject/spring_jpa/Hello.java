ee6873da5e5625203c8c5ddaa005d1abe19f3a06
hyun6ik <fkanwm@naver.com>
2021-01-11 21:58:07 +0900

    Spring + JPA

---
+++ src/main/java/springeighthproject/spring_jpa/Hello.java
@@ -0,0 +1,10 @@
+package springeighthproject.spring_jpa;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Hello {
+
+    private String data;
+}
---
+++ src/main/java/springeighthproject/spring_jpa/SpringJpaApplication.java
@@ -0,0 +1,18 @@
+package springeighthproject.spring_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaApplication {
+
+	public static void main(String[] args) {
+
+		Hello hello = new Hello();
+		hello.setData("hello");
+		String data = hello.getData();
+		System.out.println("data = " + data);
+		SpringApplication.run(SpringJpaApplication.class, args);
+	}
+
+}
