d29b1462b31d3f80e8426c2a2d0eff77add5888e
hyun6ik <fkanwm@naver.com>
2021-01-12 21:10:29 +0900

    상품 엔티티 개발(비즈니스 로직 추가)

--- src/main/java/springeighthproject/spring_jpa/domain/item/Item.java
+++ src/main/java/springeighthproject/spring_jpa/domain/item/Item.java
@@ -3,6 +3,7 @@ package springeighthproject.spring_jpa.domain.item;
 import lombok.Getter;
 import lombok.Setter;
 import springeighthproject.spring_jpa.domain.Category;
+import springeighthproject.spring_jpa.domain.exception.NotEnoughStockException;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -23,5 +24,21 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+
+    //== 비즈니스 로직 ==//
+    //재고 수량 증가 로직
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+    //재고 수량 감소 로직
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity -= quantity;
+        if(restStock <0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
+
 
 }
