9e242748c51cc66741c3cfb4aedfa8c8265dad4a
hyun6ik <fkanwm@naver.com>
2021-01-13 23:29:51 +0900

    주문 목록 검색, 취소

--- src/main/java/springeighthproject/spring_jpa/controller/OrderController.java
+++ src/main/java/springeighthproject/spring_jpa/controller/OrderController.java
@@ -3,11 +3,11 @@ package springeighthproject.spring_jpa.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import springeighthproject.spring_jpa.domain.Member;
+import springeighthproject.spring_jpa.domain.Order;
 import springeighthproject.spring_jpa.domain.item.Item;
+import springeighthproject.spring_jpa.repository.OrderSearch;
 import springeighthproject.spring_jpa.service.ItemService;
 import springeighthproject.spring_jpa.service.MemberService;
 import springeighthproject.spring_jpa.service.OrderService;
@@ -38,6 +38,20 @@ public class OrderController {
                         @RequestParam("itemId") Long itemId,
                         @RequestParam("count") int count) {
         orderService.order(memberId, itemId, count);
+        return "redirect:/orders";
+    }
+
+    @GetMapping("/orders")
+    public String orderList(@ModelAttribute("orderSearch") OrderSearch orderSearch, Model model){
+        List<Order> orders = orderService.findOrders(orderSearch);
+        model.addAttribute("orders", orders);
+        return "order/orderList";
+    }
+
+    @PostMapping("/orders/{orderId}/cancel")
+    public String cancelOrder(@PathVariable("orderId") Long orderId)
+    {
+        orderService.cancelOrder(orderId);
         return "redirect:/orders";
     }
 }
